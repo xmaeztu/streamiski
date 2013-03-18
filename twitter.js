47ee4e5b0a6d9acc7b2b846103f7cac1a08e99ee
Xabier Maeztu <x.maeztu@gmail.com>
2013-03-18 09:49:37 +0000

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -2,18 +2,18 @@ var twitter = require('ntwitter');
 var credentials = require('./credentials.js');
 
 var t = new twitter({
-      consumer_key: credentials.consumer_key,
-        consumer_secret: credentials.consumer_secret,
-        access_token_key: credentials.access_token_key,
-        access_token_secret: credentials.access_token_secret
+  consumer_key: credentials.consumer_key,
+  consumer_secret: credentials.consumer_secret,
+  access_token_key: credentials.access_token_key,
+  access_token_secret: credentials.access_token_secret
 });
 
 t.stream(
-        'statuses/filter',
-            { track: ['#Los132QueFaltan'] },
-                function(stream) {
-                          stream.on('data', function(tweet) {
-                                        console.log(tweet.text);
-                                                });
-                              }
-    );
+  'statuses/filter',
+  { track: ['#Los132QueFaltan'] },
+  function(stream) {
+    stream.on('data', function(tweet) {
+      console.log(tweet.text);
+    });
+  }
+);
